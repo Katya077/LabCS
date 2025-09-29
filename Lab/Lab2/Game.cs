20fad2bf59ed7ce5a36d7eac759e6dc402ba3ad9
Екатерина Слесарчик <slesarchikkaterina@gmail.com>
2025-09-29 20:40:39 +0300

    add method GetDistance

--- Lab/Lab2/Game.cs
+++ Lab/Lab2/Game.cs
@@ -15,18 +15,27 @@ public class Game
     private Player mouse;
     private int size;
     private GameState state;
-    
+
     private List<(int catLoc, int mouseLoc, int distatce)> history;
 
     public Game(int size)
     {
-       this.size = size;
+        this.size = size;
         cat = new Player("Cat");
         mouse = new Player("Mouse");
         state = GameState.Start;
         history = new List<(int catLoc, int mouseLoc, int distatce)>();
-        
+
     }
 
-    
+    private int GetDistance()
+    {
+        if (cat.State == State.NotInGame || mouse.State == State.NotInGame)
+            return -1;
+        int d = Math.Abs(cat.Position - mouse.Position);
+        return Math.Min(d, size - d);
+    }
+
+
+
 }
