1f59d31de4cf7a62201ee6f0a4ab2eded97c0c75
Екатерина Слесарчик <slesarchikkaterina@gmail.com>
2025-09-29 16:10:02 +0300

    create constructor and fields

--- Lab/Lab2/Game.cs
+++ Lab/Lab2/Game.cs
@@ -2,5 +2,14 @@
 
 public class Game
 {
-    
+    private Player cat;
+    private Player mouse;
+    private int NumofCells;
+
+    public Game(int numofCells)
+    {
+        NumofCells = cells;
+        cat = new Player("Cat");
+        mouse = new Player("Mouse");
+    }
 }
