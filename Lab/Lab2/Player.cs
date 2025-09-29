77c0b835e3284af785b12ab8f9d38b7c659caa4e
Екатерина Слесарчик <slesarchikkaterina@gmail.com>
2025-09-29 20:22:38 +0300

    update class Player

--- Lab/Lab2/Player.cs
+++ Lab/Lab2/Player.cs
@@ -12,30 +12,28 @@ public enum State
 
 public class Player
 {
-public string Name { get; set; }
-public int Position { get; set; }
-public string State { get; set; }
-public int DistanceTraveled { get; set; }
+    public string Name { get; set; }
+    public int Position { get; set; }
+    public string State { get; set; }
+    public int DistanceTraveled { get; set; }
 
-public Player(string name)
-{
-    Name = name;
-    State = "NotInGame";
-    Position = 0;
-    DistanceTraveled = 0;
-}
+    public Player(string name)
+    {
+        Name = name;
+        Position = -1;
+    }
 
-public void SetPosition(int pos)
-{
-    Position = pos;
-    State = State.Playing;
-}
-
-public void Move(int steps, int size)
-{
-    if (State == State.NotInGame) return;
-     Position = (Position + steps) % size;
-     if(Position < 0) Position += size;
-     DistanceTraveled += Math.Abs(steps);
-}
+    public void SetPosition(int pos)
+    {
+        Position = pos;
+        State = State.Playing;
+    }
+    public void Move(int steps, int size)
+    {
+        if (State == State.NotInGame) return;
+        Position = (Position + steps) % size;
+        
+        if (Position < 0) Position += size;
+        DistanceTraveled += Math.Abs(steps);
+    }
 }
