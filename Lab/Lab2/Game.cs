348e837d8c39daf323f9cf91803e55fd9ed8647d
Екатерина Слесарчик <slesarchikkaterina@gmail.com>
2025-09-29 20:03:15 +0300

    add enum, update class game

--- Lab/Lab2/Game.cs
+++ Lab/Lab2/Game.cs
@@ -1,15 +1,32 @@
-﻿namespace Lab2;
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab2;
+
+public enum GameState
+{
+    Start,
+    End
+}
 
 public class Game
 {
     private Player cat;
     private Player mouse;
-    private int NumofCells;
+    private int size;
+    private GameState state;
+    
+    private List<(int catLoc, int mouseLoc, int distatce)> history;
 
-    public Game(int numofCells)
+    public Game(int size)
     {
-        NumofCells = cells;
+       this.size = size;
         cat = new Player("Cat");
         mouse = new Player("Mouse");
+        state = GameState.Start;
+        history = new List<(int catLoc, int mouseLoc, int distatce)>();
+        
     }
+
+    
 }
