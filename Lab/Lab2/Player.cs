1fd0dc264a241769c1548c15efe7187ee081ffe2
Екатерина Слесарчик <slesarchikkaterina@gmail.com>
2025-09-29 16:19:51 +0300

    add features of player

--- Lab/Lab2/Player.cs
+++ Lab/Lab2/Player.cs
@@ -2,5 +2,9 @@
 
 public class Player
 {
-    
+public string Name { get; set; }
+public int Position { get; set; }
+public string State { get; set; }
+public int DistanceTraveled { get; set; }
+
 }
