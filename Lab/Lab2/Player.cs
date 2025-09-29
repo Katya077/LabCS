e635266eee08bf8113d166902448e2e28e196d57
Екатерина Слесарчик <slesarchikkaterina@gmail.com>
2025-09-29 16:22:38 +0300

    add constructor player

--- Lab/Lab2/Player.cs
+++ Lab/Lab2/Player.cs
@@ -7,4 +7,12 @@ public int Position { get; set; }
 public string State { get; set; }
 public int DistanceTraveled { get; set; }
 
+public Player(string name)
+{
+    Name = name;
+    State = "NotInGame";
+    Position = 0;
+    DistanceTraveled = 0;
+}
+
 }
