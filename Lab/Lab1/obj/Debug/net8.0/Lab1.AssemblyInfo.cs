0b774e84c3f1320a74292c8ea0373a3bc9d35794
Екатерина Слесарчик <slesarchikkaterina@gmail.com>
2025-09-29 15:14:40 +0300

    create lab2

--- Lab/Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
+++ Lab/Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac79304744d499bfa4a373151edb9bc8390c0a8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c19246bbf37731b3470951e0ad97127f48aac80")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
