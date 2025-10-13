72bb904f9a80308c7d010d5db8d3f0eb48cdeed1
Екатерина Слесарчик <slesarchikkaterina@gmail.com>
2025-10-13 19:36:09 +0300

    create clases, add methods

--- Lab/Lab3/Text.cs
+++ Lab/Lab3/Text.cs
@@ -14,4 +14,23 @@ public class Text
         Sentences.Add(sentence);
     }
     
+    public void PrintAllSentences()
+    {
+       Console.WriteLine("\nВсе предложения: ");
+       for (int i = 0; i < Sentences.Count; i++)
+       {
+           Console.WriteLine($"Предложение {i+1}: {Sentences[i]}");
+       }
+    }
+    public void PrintAllWords()
+    {
+        Console.WriteLine("\nВсе слова текста: ");
+        foreach (var sentence in Sentences)
+        {
+            foreach (var word in sentence.GetWords())
+            {
+                Console.WriteLine($"Слово: {word}");
+            }
+        }
+    }
 }
---
+++ Lab/Lab3/Word.cs
@@ -0,0 +1,16 @@
+﻿namespace Lab3;
+
+public class Word
+{
+    public string Value { get; private set; }
+
+    public Word(string value)
+    {
+        Value = value;
+    }
+
+    public override string ToString()
+    {
+        return Value;
+    }
+}
